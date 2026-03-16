0c4159b92689df950a060ca188e002267c24aaec
Cassandre-Chardron <kacendre007@gmail.com>
2026-03-16 17:12:53 +0100

    Create Fiche_enseignant_informations.php

---
+++ public/Fiche_enseignant_informations.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Types Intervention</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calendrier</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <nav>
+        // en cours par Chloé
+    </nav>
+    <section class="intervention-type">
+        <div class="breadcrumb">
+            <img src="assets/home.png" alt="">
+            <p>></p>
+            <p>Corps enseignant</p>
+            <p>></p>
+        </div>
+
+        <section class="titles-page">
+            <div class="align">
+                <h3>Corps enseignant</h3>
+                <div class="button">
+                    <button command="show-modal" commandfor="dialog" class="blue-button">Ajouter un type</button>
+                </div>
+            </div>
+
+            <form method="post" action="">
+                <h3 class="yellow">Filtres</h3>
+                <div class="intervention-row">
+                    <div class="intervention-column">
+                        <label name="name-filter">Nom</label>
+                        <input type="text" name="name-filter" placeholder="Saisissez le nom">
+                    </div>
+                    <button class="yellow-button">Filtrer</button>
+                </div>
+            </form>
+        </section>
+    </section>
+</body>
+</html>
