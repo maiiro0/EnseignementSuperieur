2fe4811e04f44fb5b89438beed85e456f7995bff
Cassandre-Chardron <kacendre007@gmail.com>
2026-03-19 00:01:55 +0100

    forme fiche enseignants onglet informations
    Forme détailler du code de la page ficheenseignant onglet information avec une partie du css

--- public/Fiche_enseignant_informations.php
+++ public/Fiche_enseignant_informations.php
@@ -31,6 +31,9 @@
             <p>></p>
             <p>Corps enseignant</p>
             <p>></p>
+            <p>(var)</p>
+            <p>></p>
+            <p>Corps enseignant</p>
         </div>
 
         <section class="titles-page">
@@ -39,8 +42,59 @@
             </div>
                 <h3 class="yellow">Modules enseignés</h3>
                 <div class="">
-                    
-                    
+                <?php
+                    $requete = $con->prepare("SELECT u.first_name, u.last_name,m.name AS module, m.hours_count FROM instructor i JOIN user u ON i.user_id =u.id JOIN instructor_module im ON im.instructor_id = i.id JOIN module m ON im.module_id = m.id");
+                    $requete->execute();
+                    $contenu = $requete->fetchAll(\PDO::FETCH_ASSOC);
+
+                    foreach ($contenu as $valeurs=>$element) {
+                        echo "<span>". $element["last_name"]. "</span>"; 
+                        echo"<span>". ":". "</span>";
+                        echo"<td>". $element['module']. "</td>";
+                        echo"<br>";
+                    }
+                ?>  
+                </div>
+                <div></div>
+        </section>
+        <section>
+            <div>
+                <a href="">Informations générales</a>
+                <a href="">Interventions</a>
+            </div>
+            <div>
+                <h3 class="yellow">Informations générales</h3>
+            </div>
+            <form action="" method="post">
+                <div class="form-align">
+                    <div>
+                        <label for="title">Titre</label> </br>
+                        <input type="text" placeholder="Saisissez un titre sur l'intervention" name="title" id="title"></br>
+                    </div>
+                    <div>
+                        <label for="title">Titre</label> </br>
+                        <input type="text" placeholder="Saisissez un titre sur l'intervention" name="title" id="title"></br>
+                    </div>
+                    <div>
+                        <label for="title">Titre</label> </br>
+                        <input type="text" placeholder="Saisissez un titre sur l'intervention" name="title" id="title"></br>
+                    </div>   
+                </div>
+
+                <label for="inter">Intervenant - champ obligatoire</label></br>
+                <select name="inter" id="inter">
+                        <option value="">Sélectionner des intervenants</option>
+                        <?php
+                            $requete = $con->prepare("SELECT upper(last_name), first_name FROM user ORDER BY last_name");
+                            $requete->execute();
+                            $nom_intervenants = $requete->fetchAll(\PDO::FETCH_ASSOC);
+                            foreach ($nom_intervenants as $valeurs=>$element) { 
+                                echo "<option>". $element["upper(last_name)"]. " ". $element["first_name"] ."</option>";
+                            }
+                        ?>
+                </select></br>
+                <div class="select-button">
+                    <button type="submit" class="blue-button selection">Confirmer</button>
                 </div>
             </form>
         </section>
