0d238565a6554ca480a608d475bde92146986d0a
Hugo Plus <romainplus777@gmail.com>
2026-03-20 11:13:59 +0100

    Indentation du html de Corps-Enseignants

--- public/Corps-Enseigants.php
+++ public/Corps-Enseigants.php
@@ -23,69 +23,67 @@ $enseignants = [
 
 <body>
 
-<div class="container">
+    <div class="container">
 
-<div class="top-bar">
-<h1>Corps enseignant</h1>
-<button class="button-primary">
-Ajouter un enseignant
-</button>
-</div>
+    <div class="top-bar">
+        <h1>Corps enseignant</h1>
+        <button class="button-primary">
+            Ajouter un enseignant
+        </button>
+    </div>
 
-<div class="card">
-<h3 class="card-title">Filtres</h3>
+    <div class="card">
+        <h3 class="card-title">Filtres</h3>
 
-<form class="filters-teachers">
-<input class="input-field" type="text" placeholder="Nom">
-<input class="input-field" type="text" placeholder="Prénom">
-<input class="input-field" type="email" placeholder="Email">
+        <form class="filters-teachers">
+            <input class="input-field" type="text" placeholder="Nom">
+            <input class="input-field" type="text" placeholder="Prénom">
+            <input class="input-field" type="email" placeholder="Email">
 
-<button class="button-secondary">
-Filtrer
-</button>
-</form>
-</div>
+            <button class="button-secondary">
+                Filtrer
+            </button>
+        </form>
+    </div>
 
-<div class="card">
-<div class="result-text">
-1 enseignant trouvé
-</div>
+    <div class="card">
+        <div class="result-text">
+            1 enseignant trouvé
+        </div>
 
-<table>
-<thead>
-<tr>
-<th>Nom de famille</th>
-<th>Prénom</th>
-<th>Modules enseignés</th>
-<th>Nombre d'heures</th>
-<th></th>
-</tr>
-</thead>
+        <table>
+            <thead>
+                <tr>
+                    <th>Nom de famille</th>
+                    <th>Prénom</th>
+                    <th>Modules enseignés</th>
+                    <th>Nombre d'heures</th>
+                    <th></th>
+                </tr>
+            </thead>
 
-<tbody>
+            <tbody>
 
-<?php foreach($enseignants as $e): ?>
+                <?php foreach($enseignants as $e): ?>
 
-<tr>
-<td><?= $e["nom"] ?></td>
-<td><?= $e["prenom"] ?></td>
-<td class="table-modules"><?= $e["modules"] ?></td>
-<td class="table-hours"><?= $e["heures"] ?></td>
+                <tr>
+                    <td><?= $e["nom"] ?></td>
+                    <td><?= $e["prenom"] ?></td>
+                    <td class="table-modules"><?= $e["modules"] ?></td>
+                    <td class="table-hours"><?= $e["heures"] ?></td>
 
-<td>
-<a class="link-primary" href="#">
-👁 Accéder à la fiche
-</a>
-</td>
-</tr>
+                    <td>
+                        <a class="link-primary" href="#">
+                            👁 Accéder à la fiche
+                        </a>
+                    </td>
+                </tr>
 
-<?php endforeach; ?>
+                <?php endforeach; ?>
 
-</tbody>
-</table>
-
-</div>
-
+            </tbody>
+        </table>
+    </div>
 </div>
 
 </body>
