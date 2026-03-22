f662029c20633921c92dd76d8a3ec7bc71d91d0a
Cassandre-Chardron <kacendre007@gmail.com>
2026-03-22 18:48:42 +0100

    Fiche enseignant partie fonctionnel
     toutes les fonctions d'affichage de la base de donnée et celle de modification.  (reste : le css et le champs à choix multiple)

--- public/Fiche_enseignant_informations.php
+++ public/Fiche_enseignant_informations.php
@@ -1,16 +1,25 @@
+
+<?php 
+require_once 'Connexion.php';
+
+/*
+if (isset($_GET['id'])) {
+    $id = htmlspecialchars($_GET['id']);
+    $requete = $con->prepare("SELECT email, last_name, first_name FROM user WHERE id=1");
+    // $requete->bindParam(':id', $id);
+    $requete->execute();
+    $infos = $requete ->fetchAll(\PDO::FETCH_ASSOC);
+}
+*/
+$id =1;
+$requete = $con->prepare("SELECT email, last_name, first_name FROM user WHERE id=:id");
+$requete->bindParam(':id', $id);
+$requete->execute();
+$infos = $requete->fetch(PDO::FETCH_ASSOC);
+
+?>
+
 <!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Types Intervention</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-</head>
-<body>
-    <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
@@ -23,39 +32,42 @@
 </head>
 <body>
     <nav>
-        // en cours par Chloé
+        <?php include_once 'Menu_gestion_licence.php' ?>
     </nav>
     <section class="intervention-type">
-        <div class="breadcrumb">
+        <div class="breadcrumb">  
             <img src="assets/home.png" alt="">
             <p>></p>
             <p>Corps enseignant</p>
             <p>></p>
-            <p>(var)</p>
+            <p><?php echo $infos["first_name"] ?></p>
+            <p><?php echo $infos["last_name"] ?></p>
             <p>></p>
-            <p>Corps enseignant</p>
+            <p>Informations générales</p>
         </div>
 
         <section class="titles-page">
             <div class="align">
-                <h3>Corps enseignant  (var)</h3>
+                <h3><span><?php echo $infos["first_name"] ?></span>
+                <span><?php echo $infos["last_name"] ?></span></h3>
             </div>
-                <h3 class="yellow">Modules enseignés</h3>
-                <div class="">
-                <?php
-                    $requete = $con->prepare("SELECT u.first_name, u.last_name,m.name AS module, m.hours_count FROM instructor i JOIN user u ON i.user_id =u.id JOIN instructor_module im ON im.instructor_id = i.id JOIN module m ON im.module_id = m.id");
-                    $requete->execute();
-                    $contenu = $requete->fetchAll(\PDO::FETCH_ASSOC);
+            <h3 class="yellow">Modules enseignés</h3>
+            <div class="">
+            <?php
+                $requete = $con->prepare("SELECT m.name, m.hours_count FROM instructor_module im JOIN module m ON im.module_id = m.id  WHERE im.instructor_id= $id ");
+                $requete->execute();
+                $contenu = $requete->fetchAll(\PDO::FETCH_ASSOC);
 
-                    foreach ($contenu as $valeurs=>$element) {
-                        echo "<span>". $element["last_name"]. "</span>"; 
-                        echo"<span>". ":". "</span>";
-                        echo"<td>". $element['module']. "</td>";
-                        echo"<br>";
-                    }
-                ?>  
-                </div>
-                <div></div>
+                foreach ($contenu as $colonne => $element) {
+                    echo"<p>";
+                    echo "<span>". $element["name"]. "</span>"; 
+                    echo"<span>". " : ". "</span>";
+                    echo"<span>". $element['hours_count']. "</span>";
+                    echo"<span>"."h00". "</span>" ;
+                    echo"</p>";
+                }
+            ?>  
+            </div>
         </section>
         <section>
             <div>
@@ -68,36 +80,67 @@
             <form action="" method="post">
                 <div class="form-align">
                     <div>
-                        <label for="title">Titre</label> </br>
-                        <input type="text" placeholder="Saisissez un titre sur l'intervention" name="title" id="title"></br>
+                        <label for="last_name">Nom de famille - champ obligatoire</label> </br>
+                        <input type="text" placeholder=" <?php echo $infos["last_name"] ?>" name="last_name" id="last_name"></br>
                     </div>
                     <div>
-                        <label for="title">Titre</label> </br>
-                        <input type="text" placeholder="Saisissez un titre sur l'intervention" name="title" id="title"></br>
+                        <label for="first_name">Prénom - champ obligatoire</label> </br>
+                        <input type="text" placeholder=" <?php echo $infos["first_name"] ?>" name="first_name" id="first_name"></br>
                     </div>
                     <div>
-                        <label for="title">Titre</label> </br>
-                        <input type="text" placeholder="Saisissez un titre sur l'intervention" name="title" id="title"></br>
+                        <label for="email">Email - champ obligatoire</label> </br>
+                        <input type="text" placeholder=" <?php echo $infos["email"] ?>" name="email" id="email"></br>
                     </div>   
                 </div>
 
-                <label for="inter">Intervenant - champ obligatoire</label></br>
-                <select name="inter" id="inter">
-                        <option value="">Sélectionner des intervenants</option>
+                <label for="name">Modules enseignés - champ obligatoire</label></br>
+                <select name="name[]" id="name" multiple>
                         <?php
-                            $requete = $con->prepare("SELECT upper(last_name), first_name FROM user ORDER BY last_name");
+                            $requete = $con->prepare("SELECT m.name FROM  module m;");
                             $requete->execute();
                             $nom_intervenants = $requete->fetchAll(\PDO::FETCH_ASSOC);
                             foreach ($nom_intervenants as $valeurs=>$element) { 
-                                echo "<option>". $element["upper(last_name)"]. " ". $element["first_name"] ."</option>";
+                                echo "<option>". $element["name"]."</option>";
                             }
                         ?>
                 </select></br>
                 <div class="select-button">
-                    <button type="submit" class="blue-button selection">Confirmer</button>
+                    <button type="submit" class="blue-button selection">Enregistrer les informations</button>
                 </div>
             </form>
         </section>
     </section>
 </body>
 </html>
+
+<?php
+if ((!empty($_POST['last_name'])) && !empty($_POST['first_name']) && !empty($_POST['email'])) {
+    $last_name = htmlspecialchars($_POST['last_name']);
+    $first_name = htmlspecialchars($_POST['first_name']);
+    $email = htmlspecialchars($_POST['email']);
+
+    $requete = $con->prepare("UPDATE user SET last_name = :last_name, first_name = :first_name, email = :email WHERE id=$id");
+    $requete->bindParam(':last_name', $last_name);
+    $requete->bindParam(':first_name', $first_name);
+    $requete->bindParam(':email', $email);
+    $requete->execute();
+
+    if(!empty($_POST['name'])){
+        $name = $_POST['name'];
+
+        $requete = $con->prepare("DELETE FROM instructor_module im  WHERE im.instructor_id=$id");
+        $requete->execute();
+    
+        foreach ($name as $colonne => $element) {
+            $requete = $con->prepare("SELECT m.id FROM module m  WHERE m.name= $element");
+            $requete->execute();
+            $module_id = $requete->fetchColumn();
+
+            $requete = $con->prepare("INSERT INTO instructor_module VALUES ($id ,$module_id)" );
+            $requete->execute();
+        }
+
+    }
+}
+
+?>
