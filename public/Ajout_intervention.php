63420e2b05aedb8fecac961a7a427c0f28e442e7
Elena Longuet <elenalonguet7@gmail.com>
2026-03-23 16:33:05 +0100

    Ajout_intervention

---
+++ public/Ajout_intervention.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Types Intervention</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calendrier</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <nav>
+        <?php require_once('Menu_gestion_licence.php'); ?>
+    </nav>
+
+    <section class="intervention-type">
+        <div class="breadcrumb">
+            <img src="assets/home.png" alt="">
+            <p>></p>
+            <p>Ajouter type intervention</p>
+        </div>
+    
+    <section class="intervention_sheet">
+        <h3>Intervention</h3>
+        <form action="" method="post">
+            <div class="form-align">
+                <div>
+                    <label for="" name="name" require>Nom - champ obligatoire</label>
+                    <input type="text" name="name" placeholder='Saisir un nom'>
+                </div>
+                <div>
+                    <label for="" name="color" require>Code couleur (hexadécimal) - champ obligatoire</label>
+                    <input type="text" name="color" placeholder='#DG451F'>
+                </div>
+            </div>
+            <div class="desc_form_update">
+                <label for="" name="description" require>Description - champ obligatoire</label>
+                <input class="input_desc" type="text" name="description" placeholder="L'intervention consiste en...">
+            </div>
+
+            <div class="button-intervention">
+                <a href="Type_intervention.php" class="grey-button selection">Retour à la liste</a>
+                <button type="submit" class="blue-button selection">Ajouter l'intervenant</button>
+            </div>
+        </form>
+    </section>
+</body>
+</html>
+
+
+<?php
+require_once 'Connexion.php';
+if ((!empty($_POST['name'])) && !empty($_POST['color']) && !empty($_POST['description'])) {
+    $name = htmlspecialchars($_POST['name']);
+    $color = htmlspecialchars($_POST['color']);
+    $description = htmlspecialchars($_POST['description']);
+
+    $requete = $con->prepare("INSERT INTO intervention_type (name, description, color) VALUES (:name, :description, :color);");
+    $requete->bindParam(':name', $name);
+    $requete->bindParam(':color', $color);
+    $requete->bindParam(':description', $description);
+    $requete->execute();
+}
+
+?>
