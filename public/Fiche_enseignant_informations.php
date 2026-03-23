8c064721e56bb5853388d26e7f355d592b9cdaa0
Cassandre-Chardron <kacendre007@gmail.com>
2026-03-23 19:09:47 +0100

    changement select -selected  fiche enseignant information
     ajout de la présélection des modules enseignées

--- public/Fiche_enseignant_informations.php
+++ public/Fiche_enseignant_informations.php
@@ -100,9 +100,18 @@ $infos = $requete->fetch(PDO::FETCH_ASSOC);
                             <?php
                                 $requete = $con->prepare("SELECT m.name FROM  module m;");
                                 $requete->execute();
-                                $nom_intervenants = $requete->fetchAll(\PDO::FETCH_ASSOC);
-                                foreach ($nom_intervenants as $valeurs=>$element) { 
-                                    echo "<option selected>". $element["name"]."</option>";
+                                $nom_module = $requete->fetchAll(\PDO::FETCH_ASSOC);
+
+                                $requete = $con->prepare("SELECT m.name FROM  module m JOIN instructor_module im ON m.id = im.module_id WHERE im.instructor_id = :id");
+                                $requete->bindParam(':id', $id);
+                                $requete->execute();
+                                $nom_module_selected = $requete->fetchAll(\PDO::FETCH_ASSOC);
+                                foreach ($nom_module as $valeurs=>$element) { 
+                                    if (in_array($element, $nom_module_selected)) {
+                                        echo "<option selected>". $element["name"]."</option>";
+                                    }else {
+                                        echo "<option>". $element["name"]."</option>";
+                                    }
                                 }
                             ?>
                     </select>
